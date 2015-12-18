2674169a30b045e73278c2d75792c7820f107ab6
Sean Cheatham <seancheatham@gmail.com>
2015-12-18 00:00:26 -0500

    Initial Commit

---
+++ src/app/components/main.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import RaisedButton from 'material-ui/lib/app-bar';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
+import Colors from 'material-ui/lib/styles/colors';
+import Header from './header';
+
+const Main = React.createClass({
+
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getInitialState() {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(LightRawTheme),
+    };
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: this.state.muiTheme,
+    };
+  },
+
+  componentWillMount() {
+    let newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
+      accent1Color: Colors.deepOrange500,
+    });
+
+    this.setState({muiTheme: newMuiTheme});
+  },
+
+  _handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  },
+
+  _handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  },
+
+  render() {
+    return (
+        <Header />
+    );
+  },
+});
+
+export default Main;
